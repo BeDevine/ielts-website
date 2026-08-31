3074705b3b7973a711c81b42a8ce5f2191efa43e
BeDevine <143723308+BeDevine@users.noreply.github.com>
2026-08-31 13:42:23 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+```tsx
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -30,6 +32,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="font-body antialiased">{children}</body>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-BPRMVE5ZYY"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BPRMVE5ZYY');
+        `}
+      </Script>
     </html>
   );
 }
+```
