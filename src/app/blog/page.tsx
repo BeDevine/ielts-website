7b95303270bede5bbd9c0af131a272e013097863
Barry Devine <barrydevine@BarryDevinesMBP.lan>
2026-09-26 08:15:00 +0700

    Support scheduled posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -17,7 +17,7 @@ export const dynamic = "force-dynamic";
 
 export default async function BlogIndexPage() {
   const posts = await db.post.findMany({
-    where: { published: true },
+    where: { published: true, createdAt: { lte: new Date() } },
     orderBy: { createdAt: "desc" },
   });
 
